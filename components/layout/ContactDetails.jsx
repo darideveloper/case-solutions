93810224c20dfb66acd2414dcc9134f4ee87962c
Silvia <silviaislam.eera@gmail.com>
2025-03-26 15:19:11 +0600

    add: contactdetails

---
+++ components/layout/ContactDetails.jsx
@@ -0,0 +1,86 @@
+import Button from "../ui/Button";
+
+function ContactDetails() {
+  return (
+    <div className="relative p-6 md:p-0 md:max-w-7xl mx-auto flex flex-col gap-x-10 gap-y-32 md:flex-row md:justify-between">
+      <div className="text-primary max-w-xl mx-auto">
+        <h1 className="mb-9 text-xl font-bold italic md:text-[32px]">
+          Disponibles para usted en:
+        </h1>
+        <div className="mb-10">
+          <h1 className="mb-4.5 text-xl font-semibold">
+            WTC - World Trade Center
+          </h1>
+          <div className="flex items-center gap-x-8">
+            <img src={"/images/icons/location-glob.svg"} />
+            <p>
+              Montecito 38 piso 28 Oficina 12 Colonia Nápoles , C.P. 03810.
+              Delegación Benito Juárez México, CDMX.
+            </p>
+          </div>
+        </div>
+        <div>
+          <h1 className="mb-4.5 text-xl font-semibold">
+            Estamos a sus órdenes de:
+          </h1>
+          <div className="flex items-center gap-x-8">
+            <img src={"/images/icons/hours-clock.svg"} />
+            <p>Lunes a Viernes de 9:00 a 18:00 Sábado de 9:00 a 13:00</p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full max-w-xl mx-auto">
+        <h1 className="text-primary mb-9 text-xl font-bold italic md:text-[32px]">
+          Formulario
+        </h1>
+        <form action="#" className="flex flex-col items-center gap-y-13">
+          <div className="flex w-full items-center gap-x-3.5">
+            <img src={"/images/icons/contact.svg"} className="h-10 w-10" />
+            <input
+              type="text"
+              placeholder="Nombre Completo*"
+              className="border-primary text-primary w-full border-b p-2 font-semibold focus:outline-none"
+            />
+          </div>
+          <div className="flex w-full items-center gap-x-3.5">
+            <img src={"/images/icons/message-glob.svg"} className="h-10 w-10" />
+            <input
+              type="text"
+              placeholder="Empresa"
+              className="border-primary text-primary w-full border-b p-2 font-semibold focus:outline-none"
+            />
+          </div>
+          <div className="flex w-full items-center gap-x-3.5">
+            <img src={"/images/icons/email.svg"} className="h-10 w-10" />
+            <input
+              type="email"
+              placeholder="Email*"
+              className="border-primary text-primary w-full border-b p-2 font-semibold focus:outline-none"
+            />
+          </div>
+          <div className="flex w-full items-center gap-x-3.5">
+            <img src={"/images/icons/whatsapp.svg"} className="h-10 w-10" />
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder="Telefpono*"
+              className="border-primary text-primary w-full border-b p-2 font-semibold focus:outline-none"
+            />
+          </div>
+          <div className="flex w-full items-center gap-x-3.5">
+            <img src={"/images/icons/message.svg"} className="h-10 w-10" />
+            <input
+              type="text"
+              placeholder="Mensaje*"
+              className="border-primary text-primary w-full border-b p-2 font-semibold focus:outline-none"
+            />
+          </div>
+          <Button variant="primary">Enviar</Button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ContactDetails;
--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,3 +1,4 @@
+import ContactDetails from "@/components/layout/ContactDetails";
 import Hero from "@/components/layout/Hero";
 import Hobelmos from "@/components/layout/Hobelmos";
 import Navbar from "@/components/layout/Navbar";
@@ -8,10 +9,12 @@ export default function Contact() {
       <Navbar />
       <Hero title="Contactanos" image={"/images/hero-contact.png"} />
       <Hobelmos title={"Contactanos"}>
-        Envíenos un correo electrónico a <span className="font-bold">hamaro@casesolutions.com.mx</span> o complete
-        el siguiente formulario y en breve un miembro de nuestro equipo se
-        pondrá en contacto con usted,
+        Envíenos un correo electrónico a{" "}
+        <span className="font-bold">hamaro@casesolutions.com.mx</span> o
+        complete el siguiente formulario y en breve un miembro de nuestro equipo
+        se pondrá en contacto con usted,
       </Hobelmos>
+      <ContactDetails />
     </div>
   );
 }
