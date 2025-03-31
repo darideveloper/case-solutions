4c974f1319a9e837ccc6e1268e3c5f46794ff7ef
Silvia <silviaislam.eera@gmail.com>
2025-04-01 01:19:08 +0600

    fix: savehr

--- components/layout/Savehr.jsx
+++ components/layout/Savehr.jsx
@@ -1,7 +1,7 @@
 // ui elements
 import Button from "@/components/ui/Button";
 
-export default function Savehr({ className='' }) {
+export default function Savehr({ className = "" }) {
   return (
     <section
       className={`relative overflow-hidden bg-white py-[50px] ${className}`}
@@ -13,12 +13,10 @@ export default function Savehr({ className='' }) {
       }}
     >
       <div className="container mx-auto px-4 md:px-6 lg:px-8 relative z-10 pb-32 pt-16">
-        <div className="max-w-3xl mx-auto text-center">
-          <h2 className="text-xl md:text-4xl italic font-bold text-primary mb-4 inline-flex">
-            Administre su flota vehicular con 
-            <span className="text-orange relative inline-block">
-              <img src="/images/savehr-logo.svg" alt="box-icon" className="p-2"/>
-            </span>
+        <div className="max-w-3xl mx-auto text-center mb-24">
+          <h2 className="text-xl md:text-4xl italic font-bold text-primary mb-4 md:inline-flex items-center">
+            Administre su flota vehicular con
+            <img src="/images/savehr-logo.svg" alt="box-icon" className="mt-4 md:mt-0 md:ml-4 inline w-40" />
           </h2>
           <p className="text-gray mb-8 max-w-xl mx-auto text p-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
