4c3acd16368baefde7526da527cebef4ceba6a80
Silvia <silviaislam.eera@gmail.com>
2025-04-02 01:04:31 +0600

    fix: add flipbook image

--- components/layout/Flipbook.jsx
+++ components/layout/Flipbook.jsx
@@ -6,13 +6,7 @@ export default function Flipbook() {
   return (
     <section className="relative overflow-hidden bg-white my-12 p-4 md:p-0 md:my-20 md:max-w-7xl mx-auto">
       <TitleText />
-      <iframe
-        src="https://catalogueselectrolux.com/electrolux-catalogue-flipsnack/full-view.html"
-        frameBorder="0"
-        width="100%"
-        height="100%"
-        className="my-18"
-      />
+      <img src="/images/flipbook-example.png" alt="" className="mb-12" />
       <div className="flex justify-center">
         <Button className="text-primary mr-4" variant="primary">
           Quiero saber más
