e05de7f3234325864e612d75fc2d0211f6e2749e
Silvia <silviaislam.eera@gmail.com>
2025-04-01 23:05:29 +0600

    fix: download card

--- components/ui/DownloadCard.jsx
+++ components/ui/DownloadCard.jsx
@@ -13,7 +13,7 @@ const DownloadCard = () => {
   ];
 
   return (
-    <div className="relative mx-auto mt-38 flex max-w-7xl flex-col-reverse bg-white shadow-lg shadow-gray md:flex-row lg:flex-row">
+    <div className="relative mx-auto mt-38 flex w-11/12 md:max-w-7xl flex-col-reverse bg-white shadow-lg shadow-gray md:flex-row lg:flex-row">
       <div className="absolute bottom-0 left-0 w-full md:relative lg:w-1/2">
         <img
           src={`/images/download-card-bg.png`}
@@ -21,16 +21,16 @@ const DownloadCard = () => {
           className="h-full w-full rounded-l-lg object-cover"
         />
       </div>
-      <div className="w-full px-4 lg:w-1/2 lg:px-8">
-        <h4 className="text-primary mt-16 mb-12 text-center text-xl font-black sm:text-2xl md:text-3xl lg:text-4xl">
+      <div className="w-full px-4 lg:w-1/2 lg:px-8 z-[-0]">
+        <h2 className="text-primary mt-16 mb-12 text-center text-2xl font-bold">
           DESCARGABLES
-        </h4>
+        </h2>
 
         <div className="grid w-full grid-cols-1 place-items-center gap-1 md:grid-cols-2 lg:place-items-start">
           {presentations.map((presentation, index) => (
             <button
               key={index}
-              className="text-primary text-left text-base md:text-lg"
+              className="text-primary text-left text-base text-xl"
             >
               {presentation}
             </button>
