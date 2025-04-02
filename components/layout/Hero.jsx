91885633ecbd2e3a3d888836b1aba482af9ad82e
daridev <darideveloper@gmail.com>
2025-04-02 12:31:19 -0600

    chore: rename hero

--- components/layout/Hero.jsx
+++ components/layout/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero({ title, image, className = "" }) {
         backgroundSize: "cover",
       }}
     >
-      <div className="max-w-7xl mx-auto flex relative justify-start items-center h-full">
+      <div className="container debug max-w-7xl mx-auto flex relative justify-start items-center h-full">
         <Title
           isH1={true}
           className="text-[40px] font-black text-white uppercase md:text-[60px] py-18 md:p-18 z-10 overflow-hidden"
