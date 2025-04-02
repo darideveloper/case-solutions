279ca398f497860285c5c8367282d816489fe68d
daridev <darideveloper@gmail.com>
2025-04-02 12:53:01 -0600

    feat (about): dummy data for ProductShowcase cards

--- pages/about.jsx
+++ pages/about.jsx
@@ -9,14 +9,37 @@ import Hero from "@/components/layout/Hero";
 import Layout from "@/components/layout/Layout";
 import QuoteDescription from "@/components/layout/QuoteDescription";
 
+const cardInfo1 = {
+  imagePosition: "left", // 'left' or 'right'
+  title: "LOREM IPSUM DOLOR 1",
+  subtitle: "NUESTRA MISIÓN",
+  description: "Posicionarse en el mercado como una de las mejores empresas de desarrollo en México y Latinoamérica mediante la plena satisfacción de los requerimientos de sus clientes, empleando al personal mejor calificado en cuanto a habilidades y conocimientos informáticos.",
+  imageSrc: "/images/about-showcase-1.png",
+  imageAlt: "Product showcase image",
+}
+
+const cardInfo2 = {
+  imagePosition: "right", // 'left' or 'right'
+  title: "LOREM IPSUM DOLOR 2",
+  subtitle: "NUESTRA VISIÓN",
+  description: "Desarrollar productos de software que permitan a las empresas mejorar sus operaciones y alcanzar sus objetivos, mediante la correcta interpretación de sus necesidades, aplicando metodologías y herramientas de vanguardia.",
+  imageSrc: "/images/about-showcase-2.png",
+  imageAlt: "Product showcase image",
+}
+
+
 export default function About() {
   return (
     <Layout>
       <Hero title={"Quiénes somos"} image={"/images/hero-about.png"} />
       <QuoteDescription />
       <AboutNav />
-      <ProductShowcase imageAlt="Product showcase image" imageSrc="/images/about-showcase-1.png" className="mb-8" />
-      <ProductShowcase imagePosition="right" imageAlt="Product showcase image" imageSrc="/images/about-showcase-2.png" />
+      <ProductShowcase 
+        {...cardInfo1}
+      />
+      <ProductShowcase
+        {...cardInfo2}
+      />
       <ContactService />
       <ClientShowcase />
     </Layout>
