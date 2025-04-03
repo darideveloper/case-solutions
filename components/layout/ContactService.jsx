e4c6b6c6fa1ac962da2df0f52c1075ff95cd2afd
daridev <darideveloper@gmail.com>
2025-04-03 08:07:38 -0600

    feat (ContactService): add container

--- components/layout/ContactService.jsx
+++ components/layout/ContactService.jsx
@@ -3,7 +3,7 @@ import HoverCard from "@/components/ui/HoverCard";
 
 export default function ContactService() {
   return (
-    <div className="max-w-7xl mx-auto mt-35 flex flex-col gap-y-5.5 md:flex-row md:gap-x-5">
+    <div className="container max-w-7xl mx-auto mt-35 flex flex-col gap-y-5.5 md:flex-row md:gap-x-5">
       <HoverCard
         title={"Nestros Servicios"}
         className={`bg-[url(/images/service-card.png)]`}
