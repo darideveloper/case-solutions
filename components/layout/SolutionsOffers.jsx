31e56dcbe560e0ca6990f4cb0302c1f1834737a2
daridev <darideveloper@gmail.com>
2025-04-03 08:01:28 -0600

    style (SolutionsOffers): align content and add container

--- components/layout/SolutionsOffers.jsx
+++ components/layout/SolutionsOffers.jsx
@@ -10,12 +10,12 @@ const features = [
 
 export default function SolutionsOffers() {
   return (
-    <div className="px-4 py-12 md:px-16 max-w-7xl mx-auto">
+    <div className="container px-4 py-12 md:px-16 max-w-7xl !mb-24">
       <h2 className="text-center text-primary font-bold text-lg md:text-2xl py-12 mb-10 uppercase">
         Consultores especializados en ofrecer soluciones de sistemas:
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-6 md:gap-y-10 md:gap-x-20 items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 max-w-lg md:max-w-4xl mx-auto items-center gap-y-6 md:gap-y-10 md:gap-x-20">
         {features.map((text, index) => (
           <div key={index} className="flex items-start gap-3">
             <Check className="text-red-600 mt-1" size={20} />
