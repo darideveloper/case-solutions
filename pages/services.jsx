6c2631bd1b9fdd7b0f7ee16f7f52813f02d205bf
daridev <darideveloper@gmail.com>
2025-04-14 10:53:11 -0600

    chore (services): remove DownloadCard

--- pages/services.jsx
+++ pages/services.jsx
@@ -26,7 +26,6 @@ export default function Services() {
       <SolutionsOffers />
       <ServiceTimeline/>
       <ServiceDropdown />
-      <DownloadCard />
       <ContactService />
       <ClientShowcase />
     </Layout>
