7d86e7329f8b716f2e065ebb94f0ed2bf1910f6f
daridev <darideveloper@gmail.com>
2025-04-14 10:46:06 -0600

    chore (SolutionsOffers): update texts

--- components/layout/SolutionsOffers.jsx
+++ components/layout/SolutionsOffers.jsx
@@ -2,17 +2,19 @@
 import { Check } from "lucide-react";
 
 const features = [
-  "Sistema desarrollado a su medida",
+  "Desarrollo de Software a la Medida",
   "Seguridad e integridad de sus datos",
   "Acceso restringido a los empleados de acuerdo a sus funciones",
   "Reingeniería de procesos para mejorar sus tiempos y metodología",
+  "Transformación digital",
+  "Integración de sistemas",
 ];
 
 export default function SolutionsOffers() {
   return (
     <div className="container px-4 py-12 md:px-16 max-w-7xl !mb-24">
       <h2 className="text-center text-primary font-bold text-lg md:text-2xl py-12 mb-10 uppercase">
-        Consultores especializados en ofrecer soluciones de sistemas:
+        Especialistas en brindar soluciones tecnológicas a la medida.
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 max-w-lg md:max-w-4xl mx-auto items-center gap-y-6 md:gap-y-10 md:gap-x-20">
