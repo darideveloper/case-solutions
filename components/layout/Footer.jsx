b7610427ebaff00427f788e8b3da023708a311e0
daridev <darideveloper@gmail.com>
2025-04-14 11:15:23 -0600

    feat (Footer): update social icons and loop rendering

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,5 +1,23 @@
 // ui elements
 import Title from "@/components/ui/Title";
+import Link from "next/link";
+import Image from "next/image";
+
+
+const socials = [
+  {
+    "link": "https://wa.me/message/EPV5DCXTRF65E1",
+    "icon": "/images/icons/whatsapp.svg",
+  },
+  {
+    "link": "https://www.facebook.com/share/1ADTQfDSw2/ ",
+    "icon": "/images/icons/facebook.svg",
+  },
+  {
+    "link": "https://x.com/CASESOLUTIONSC1?t=0T3gwTXlRvh01HFVsm1HYw&s=09 ",
+    "icon": "/images/icons/twitterx.svg",
+  },
+]
 
 const Footer = () => {
   return (
@@ -56,8 +74,22 @@ const Footer = () => {
                 </div>
               </div>
               <div className="flex justify-center gap-4 md:justify-start">
-                <img alt="twitterx" src="/images/icons/twitterx.svg" />
-                <img alt="facebook" src="/images/icons/facebook.svg" />
+                {
+                  socials.map((social, index) => (
+                    <Link
+                      key={index}
+                      href={social.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className={`
+                        duration-300
+                        hover:opacity-80
+                      `}
+                    >
+                      <Image alt="whatsapp" src={social.icon} width={30} height={30} />
+                    </Link>
+                  ))
+                }
               </div>
             </div>
             <div className="my-2 flex justify-center md:hidden">
